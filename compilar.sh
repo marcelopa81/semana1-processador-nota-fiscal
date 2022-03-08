5a7dc076e660b05cdb3dbe13555618b5ff3acb07
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2022-03-08 16:54:51 -0300

    remove scripts

--- compilar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-javac -cp ".:libs/*" -d out br/com/alura/oobj/Main.java
-
-echo "Código compilado no diretório 'out'"
--- executar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -cp "out:libs/*" br.com.alura.oobj.Main "$@"
-
